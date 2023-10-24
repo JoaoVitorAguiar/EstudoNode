25d76ceff0886d2ffd135abec2eaf23bc84d6d90
Vitor Aguiar <joaovitor4guiar@gmail.com>
2023-10-24 12:52:04 -0300

    feat: add user CRUD

---
+++ introducao-node-secs/src/controllers/UsersController.ts
@@ -0,0 +1,55 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+import {prisma} from "../lib/prisma"
+
+export class UsersController  {
+    public async list(_request : Request, response : Response) {
+        const users = await prisma.user.findMany();
+
+        return response.status(200).json(users)
+    }
+
+    public async show(request : Request, response : Response) {
+        const { id } = request.params;
+
+        const user = await prisma.user.findUnique({
+            where: {id}
+        })
+
+        return response.status(200).json(user);
+    }
+
+    public async create(request: Request, response: Response){
+        const {name, email} = request.body;
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+            },
+        });
+        return response.status(200).json(user);
+    }
+
+    public async update(request: Request, response: Response){
+        const {id} =  request.params;
+        const {name, email} = request.body;
+        const user = await prisma.user.update({
+            where: {id},
+            data: {
+                name,
+                email,
+            },
+        });
+        return response.status(200).json(user);
+    }
+
+    public async delete(request : Request, response : Response) {
+        const { id } = request.params;
+
+        const user = await prisma.user.delete({
+            where: {id}
+        })
+
+        return response.status(200).json(user);
+    }
+}
---
+++ introducao-node-secs/src/routes/usersRoutes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { UsersController } from "../controllers/UsersController";
+
+const usersRoutes = Router();
+const controller = new UsersController();
+
+
+usersRoutes.get('/list', controller.list) 
+usersRoutes.get('/show/:id', controller.show) 
+usersRoutes.post('/create', controller.create) 
+usersRoutes.put('/update/:id', controller.update)
+usersRoutes.delete('/delete/:id', controller.delete)  
+
+export {usersRoutes}
