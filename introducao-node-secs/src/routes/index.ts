84c780454df885412ba84b68e3f0f60ccbc9b1aa
Vitor Aguiar <joaovitor4guiar@gmail.com>
2023-10-24 12:49:01 -0300

    feat: add user routes

--- introducao-node-secs/src/routes/index.ts
+++ introducao-node-secs/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { helloWorldRoutes } from "./helloWordRoutes";
+import { usersRoutes } from "./usersRoutes";
 
 const routes = Router();
 
-routes.use("/hello-world", helloWorldRoutes)
+routes.use("/hello-world", helloWorldRoutes);
+routes.use("/users", usersRoutes);
 
 export {routes}
