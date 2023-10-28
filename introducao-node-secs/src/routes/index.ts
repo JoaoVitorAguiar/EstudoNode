637fa1ce0cb33beaee99b0cb1ba2f6cd0f722a19
Vitor Aguiar <joaovitor4guiar@gmail.com>
2023-10-28 16:21:46 -0300

    feat: add authenticate routes in index

--- introducao-node-secs/src/routes/index.ts
+++ introducao-node-secs/src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { helloWorldRoutes } from "./helloWordRoutes";
 import { usersRoutes } from "./usersRoutes";
+import { authenticateRoutes } from "./authenticateRoutes";
+
 
 const routes = Router();
 
 routes.use("/hello-world", helloWorldRoutes);
 routes.use("/users", usersRoutes);
+routes.use("/sessions", authenticateRoutes);
 
 export {routes}
