70d10775e18d245ab08b51fd58e191201e0c41aa
Vitor Aguiar <joaovitor4guiar@gmail.com>
2023-10-28 22:15:58 -0300

    feat: add authentication in user routes

--- introducao-node-secs/src/routes/usersRoutes.ts
+++ introducao-node-secs/src/routes/usersRoutes.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
 import { UsersController } from "../controllers/UsersController";
+import { ensureAuthenticate } from "../middlewares/ensureAuthenticate";
 
 const usersRoutes = Router();
 const controller = new UsersController();
 
+// Usuário não precisa esta autenticado
+usersRoutes.post('/create', controller.create) 
 
+usersRoutes.use(ensureAuthenticate);
+
+// Usuário precisa está autenticado
 usersRoutes.get('/list', controller.list) 
 usersRoutes.get('/show/:id', controller.show) 
-usersRoutes.post('/create', controller.create) 
 usersRoutes.put('/update/:id', controller.update)
 usersRoutes.delete('/delete/:id', controller.delete)  
 
