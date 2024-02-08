a1b497dd9ee4c9da92b1323e1cb108afebb8d831
João Vitor Aguiar <joaovitor4guiar@gmail.com>
2024-02-08 09:22:36 -0300

    feat: lesson Loading environmental variables

--- rocketseatNodeJs/03-api-solid/src/server.ts
+++ rocketseatNodeJs/03-api-solid/src/server.ts
@@ -1,8 +1,9 @@
 import {app} from "./app"
+import { env } from "./env"
 
 app.listen({
     host: "0.0.0.0", // Mais acessível para frontends
-    port: 3333
+    port: env.PORT
 }).then(()=>{
     console.log("🚀 HTTP server Running!")
 })
