9d6a2078931345fa5ad2a81a5803a39f1a265c35
João Vitor Aguiar <joaovitor4guiar@gmail.com>
2024-02-09 13:43:55 -0300

    feat: lesson Registration Controller

--- rocketseatNodeJs/03-api-solid/src/app.ts
+++ rocketseatNodeJs/03-api-solid/src/app.ts
@@ -1,24 +1,6 @@
 import fastify from "fastify";
-import { z } from 'zod';
-import { prisma } from "@/lib/prisma";
+import { appRoutes } from "./http/routes/routes";
 
 export const app = fastify();
 
-app.post('/users', async (request, replay) => {
-    const registerBodySchema = z.object({
-        name: z.string().min(6),
-        email: z.string().email(),
-        password: z.string().min(6),
-
-    })
-    const { name, email, password } = registerBodySchema.parse(request.body) // "Parse()" lança um erro, ja o parseSafe() não
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash: password
-        }
-    })
-
-    return replay.status(201).send()
-})
+app.register(appRoutes)
