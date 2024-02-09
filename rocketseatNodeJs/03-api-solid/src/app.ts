41a18050d202446d6f08ecdc3d4eca3012719b21
João Vitor Aguiar <joaovitor4guiar@gmail.com>
2024-02-09 11:07:39 -0300

    feat: lesson Creating a user

--- rocketseatNodeJs/03-api-solid/src/app.ts
+++ rocketseatNodeJs/03-api-solid/src/app.ts
@@ -1,3 +1,24 @@
-import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
+import { z } from 'zod';
+import { prisma } from "@/lib/prisma";
+
 export const app = fastify();
+
+app.post('/users', async (request, replay) => {
+    const registerBodySchema = z.object({
+        name: z.string().min(6),
+        email: z.string().email(),
+        password: z.string().min(6),
+
+    })
+    const { name, email, password } = registerBodySchema.parse(request.body) // "Parse()" lança um erro, ja o parseSafe() não
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password_hash: password
+        }
+    })
+
+    return replay.status(201).send()
+})
