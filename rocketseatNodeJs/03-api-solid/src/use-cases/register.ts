1c6151ddc0bf6512a317e3730ffee298a66191fb
João Vitor Aguiar <joaovitor4guiar@gmail.com>
2024-02-10 18:35:56 -0300

    feat: lesson Repository Pattern

--- rocketseatNodeJs/03-api-solid/src/use-cases/register.ts
+++ rocketseatNodeJs/03-api-solid/src/use-cases/register.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma"
+import { PrismaUserRepository } from "@/repositories/prisma-users-repository"
 import { hash } from "bcryptjs"
 
 interface RegisterUseCaseRequest {
@@ -19,11 +20,6 @@ export async function registerUseCase({ name, email, password }: RegisterUseCase
         throw new Error('E-mail already exists')
     }
 
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash
-        }
-    })
+    const prismaUserRepository = new PrismaUserRepository()
+    await prismaUserRepository.create({ name, email, password_hash })
 }
