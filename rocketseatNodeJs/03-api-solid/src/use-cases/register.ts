14c7669c1212174a404478561df4d1c94f1dc28a
João Vitor Aguiar <joaovitor4guiar@gmail.com>
2024-02-13 15:42:42 -0300

    feat: lesson Repository interface

--- rocketseatNodeJs/03-api-solid/src/use-cases/register.ts
+++ rocketseatNodeJs/03-api-solid/src/use-cases/register.ts
@@ -1,5 +1,4 @@
-import { prisma } from "@/lib/prisma"
-import { PrismaUserRepository } from "@/repositories/prisma-users-repository"
+import { UserRepository } from "@/repositories/user-repository"
 import { hash } from "bcryptjs"
 
 interface RegisterUseCaseRequest {
@@ -9,18 +8,14 @@ interface RegisterUseCaseRequest {
 }
 
 export class RegisterUseCase {
-    constructor(private userRepository: any) {
+    constructor(private userRepository: UserRepository) {
 
     }
 
     async execute({ name, email, password }: RegisterUseCaseRequest) {
         const password_hash = await hash(password, 6)
 
-        const userWithSameEmail = await prisma.user.findUnique({
-            where: {
-                email
-            }
-        })
+        const userWithSameEmail = await this.userRepository.findByEmail(email)
         if (userWithSameEmail) {
             throw new Error('E-mail already exists')
         }
