b3b698f09ff56dd1a78979e70891b73d5ecb5de2
João Vitor Aguiar <joaovitor4guiar@gmail.com>
2024-02-20 14:50:40 -0300

    feat: lesson Nearby gyms use case

--- rocketseatNodeJs/03-api-solid/src/repositories/in-memory/in-memory-gyms-repository.ts
+++ rocketseatNodeJs/03-api-solid/src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,7 +1,8 @@
 import { Decimal } from "@prisma/client/runtime/library";
-import { GymsRepository } from "../gyms-repository";
+import { FindManyNearbyParams, GymsRepository } from "../gyms-repository";
 import { Gym, Prisma } from "@prisma/client";
 import { randomUUID } from "node:crypto";
+import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates";
 
 export class InMemomyGymsRepository implements GymsRepository {
     public gyms: Gym[] = []
@@ -9,6 +10,19 @@ export class InMemomyGymsRepository implements GymsRepository {
         return this.gyms
             .filter((g) => g.title.includes(query))
             .slice((page - 1) * 20, page * 20)
+    }
+    async findManyNearby(params: FindManyNearbyParams) {
+        return this.gyms.filter((item) => {
+            const distance = getDistanceBetweenCoordinates(
+                { latitude: params.latitude, longitude: params.longitude },
+                {
+                    latitude: item.latitude.toNumber(),
+                    longitude: item.longitude.toNumber(),
+                },
+            )
+
+            return distance < 10
+        })
     }
     async create(data: Prisma.GymCreateInput) {
         const gym = {
