bf2d74f9c59e0bb1bb26240329ada2cd542e1bc2
João Vitor Aguiar <joaovitor4guiar@gmail.com>
2024-02-21 11:54:59 -0300

    feat: lesson Gym creation controller

--- rocketseatNodeJs/03-api-solid/src/app.ts
+++ rocketseatNodeJs/03-api-solid/src/app.ts
@@ -1,8 +1,9 @@
 import fastify from "fastify";
-import { appRoutes } from "./http/routes/routes";
+import { usersRoutes } from "./http/controllers/users/routes";
 import { ZodError } from "zod";
 import { env } from "./env";
 import fastifyJwt from "@fastify/jwt";
+import { gymRoutes } from "./http/controllers/gyms/routes";
 
 export const app = fastify();
 
@@ -10,7 +11,9 @@ export const app = fastify();
 app.register(fastifyJwt, {
     secret: env.JWT_SECRET
 })
-app.register(appRoutes)
+
+app.register(usersRoutes)
+app.register(gymRoutes)
 
 app.setErrorHandler((error, request, reply) => {
     if (error instanceof ZodError) // Erro de validação
