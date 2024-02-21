ddf32acd84f01727fc17b7ef31842e80e226ea21
João Vitor Aguiar <joaovitor4guiar@gmail.com>
2024-02-21 14:29:46 -0300

    feat: lesson Outros controllers da academia

--- rocketseatNodeJs/03-api-solid/src/http/controllers/gyms/routes.ts
+++ rocketseatNodeJs/03-api-solid/src/http/controllers/gyms/routes.ts
@@ -1,7 +1,15 @@
 import { FastifyInstance } from 'fastify'
-import { verifyJwt } from '../../middlewares/verify-jwt'
 
-export async function gymRoutes(app: FastifyInstance) {
+import { verifyJwt } from '@/http/middlewares/verify-jwt'
+import { search } from './search'
+import { nearby } from './nearby'
+import { create } from './create'
+
+export async function gymsRoutes(app: FastifyInstance) {
     app.addHook('onRequest', verifyJwt)
-}
 
+    app.get('/gyms/search', search)
+    app.get('/gyms/nearby', nearby)
+
+    app.post('/gyms', create)
+}
