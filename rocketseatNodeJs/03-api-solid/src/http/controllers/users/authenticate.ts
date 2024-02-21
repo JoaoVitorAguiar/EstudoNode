7301b720b6a80255e2285f72650a5305de579608
João Vitor Aguiar <joaovitor4guiar@gmail.com>
2024-02-21 17:31:14 -0300

    feat: add roles

--- rocketseatNodeJs/03-api-solid/src/http/controllers/users/authenticate.ts
+++ rocketseatNodeJs/03-api-solid/src/http/controllers/users/authenticate.ts
@@ -1,36 +1,66 @@
-import { FastifyRequest, FastifyReply } from "fastify"
-import { z } from "zod"
-import { InvalidCredentialsError } from "@/use-cases/erros/invalid-credentials-error"
-import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case"
-import { env } from "@/env"
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
+import { InvalidCredentialsError } from '@/use-cases/erros/invalid-credentials-error'
 
-export async function authenticate(request: FastifyRequest, replay: FastifyReply) {
-    const autenticateBodySchema = z.object({
+export async function authenticate(
+    request: FastifyRequest,
+    reply: FastifyReply,
+) {
+    const authenticateBodySchema = z.object({
         email: z.string().email(),
         password: z.string().min(6),
-
     })
-    const { email, password } = autenticateBodySchema.parse(request.body) // "Parse()" lança um erro, ja o parseSafe() não
+
+    const { email, password } = authenticateBodySchema.parse(request.body)
 
     try {
         const authenticateUseCase = makeAuthenticateUseCase()
-        const { user } = await authenticateUseCase.execute({ email, password })
-        const token = await replay.jwtSign(
-            {},
+
+        const { user } = await authenticateUseCase.execute({
+            email,
+            password,
+        })
+
+        const token = await reply.jwtSign(
+            {
+                role: user.role,
+            },
             {
                 sign: {
-                    sub: user.id
-                }
-            }
-        ) // 1° parametro: payload, 2°: sign 
-        return replay.status(200).send({
-            token
-        })
-    } catch (error) {
-        if (error instanceof InvalidCredentialsError) return replay.status(400).send({
-            message: error.message
-        })
-        throw error
-    }
+                    sub: user.id,
+                },
+            },
+        )
 
+        const refreshToken = await reply.jwtSign(
+            {
+                role: user.role,
+            },
+            {
+                sign: {
+                    sub: user.id,
+                    expiresIn: '7d',
+                },
+            },
+        )
+
+        return reply
+            .setCookie('refreshToken', refreshToken, {
+                path: '/',
+                secure: true,
+                sameSite: true,
+                httpOnly: true,
+            })
+            .status(200)
+            .send({
+                token,
+            })
+    } catch (err) {
+        if (err instanceof InvalidCredentialsError) {
+            return reply.status(400).send({ message: err.message })
+        }
+
+        throw err
+    }
 }
