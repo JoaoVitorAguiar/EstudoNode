52baf2aa416c7770e657c256a3ccbbca8b0fb6d4
João Vitor Aguiar <joaovitor4guiar@gmail.com>
2024-02-21 09:35:30 -0300

    feat: lesson Profile Controller

--- rocketseatNodeJs/03-api-solid/src/http/controllers/profile.ts
+++ rocketseatNodeJs/03-api-solid/src/http/controllers/profile.ts
@@ -1,12 +1,15 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use.case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-
-
-    await request.jwtVerify() // Já gera um erro caso o jst Bearer não tenha sido enviado no header
-
-    console.log(request.user.sub)
-
-
-    return reply.status(200).send()
+    const getUserProfile = makeGetUserProfileUseCase()
+    const { user } = await getUserProfile.execute({
+        userId: request.user.sub
+    })
+    return reply.status(200).send({
+        user: {
+            ...user,
+            password_hash: undefined,
+        },
+    })
 }
