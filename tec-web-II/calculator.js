03b92175c99998f77280c5e835e2c834b07def62
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2024-03-22 10:31:18 -0300

    feat: calculator module console application - Atividade 01

---
+++ tec-web-II/calculator.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  add: (a, b) => a + b, // Addition
+  subtract: (a, b) => a - b, // Subtraction
+  multiply: (a, b) => a * b, // Multiplication
+  divide: (a, b) => a / b, // Division
+  power: (a, b) => Math.pow(a, b), // Power
+  squareRoot: (a) => Math.sqrt(a), // Square root
+  logarithm: (a) => Math.log(a), // Logarithm
+  sine: (a) => Math.sin(a) // Sine
+};
---
+++ tec-web-II/index.js
@@ -0,0 +1,34 @@
+// index.js
+const { add, divide, logarithm, multiply, power, sine, squareRoot, subtract } = require('./calculator');
+
+const num1 = process.argv[2];
+const operation = process.argv[3];
+const num2 = process.argv[4];
+
+function callFunction(num1, operation, num2) {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    switch (operation) {
+        case add.name:
+            return add(num1, num2);
+        case subtract.name:
+            return subtract(num1, num2);
+        case multiply.name:
+            return multiply(num1, num2);
+        case divide.name:
+            return divide(num1, num2);
+        case power.name:
+            return power(num1, num2);
+        case squareRoot.name:
+            return squareRoot(num1);
+        case logarithm.name:
+            return logarithm(num1);
+        case sine.name:
+            return sine(num1);
+        default:
+            return 'Operação não suportada.';
+    }
+}
+
+console.log(`Result: ${callFunction(num1, operation, num2)}`);
